b30e6b85043f2f81ee6dc212c8b8007fda9b8cad
Ingrid Pettersson <ingrid.s.pettersson@gmail.com>
2021-01-14 13:07:28 +0100

    add minor changes

--- AwaAssignment2/Guest.cs
+++ AwaAssignment2/Guest.cs
@@ -6,11 +6,64 @@ namespace awa_assignment_2
 {
     class Guest
     {
-        public string Name { get; set; }
-        public double YearsAsFriends { get; set; }
-        public string Comment { get; set; }
-        private double surpriseSaladGrade;
+        // Private field
+        private string name;
+        // Public property
+        public string Name 
+        { 
+            get 
+            { 
+                return name; 
+            } 
+            set 
+            { 
+                name = value; 
+            } 
+        }
 
+        private double yearsAsFriends;
+        public double YearsAsFriends 
+        { 
+            get 
+            { 
+                return yearsAsFriends; 
+            } 
+            set 
+            { 
+                yearsAsFriends = value; 
+            } 
+        }
+
+        private string comment;
+        public string Comment 
+        { 
+            get 
+            { 
+                return comment; 
+            } 
+            set 
+            { 
+                comment = value; 
+            } 
+        }
+
+        private double surpriseSaladGrade;
+        public double SurpriseSaladGrade
+        {
+            get 
+            { 
+                return surpriseSaladGrade; 
+            }
+            set
+            {
+                if (value <= 5 && value >= 0)
+                    surpriseSaladGrade = value;
+                else
+                    surpriseSaladGrade = -1;
+            }
+        }
+
+        // Constructor
         public Guest(string name, double yearsAsFriends)
         {
             this.Name = name;
@@ -18,6 +71,8 @@ namespace awa_assignment_2
 
             Console.WriteLine($"NAME: {name}\nYEARS AS FRIENDS: {yearsAsFriends}\n");
         }
+
+        // Constructor
         public Guest(string name, double surpriseSaladGrade, string comment)
         {
             this.Name = name;
@@ -26,27 +81,11 @@ namespace awa_assignment_2
 
             string areYouWelcome;
             if (WelcomeAgain())
-            {
-            areYouWelcome = "You can count on an invitation next time around!";
-            }
+                areYouWelcome = "Well well well, see you next time!";
             else
-            {
-                areYouWelcome = "I'm sorry you didn't like my surprise salad. So I think our roads diverse here...";
-            }
+                areYouWelcome = "You rather eat apple alone next time?";
 
             Console.WriteLine($"NAME: {name}\nGRADE: {surpriseSaladGrade}\nCOMMENT: {comment}\nCOMMENT FROM YOU TO GUEST: {areYouWelcome}\n");
-        }
-
-        public double SurpriseSaladGrade
-        {
-            get { return surpriseSaladGrade; }
-            set
-            {
-                if (value <= 5 && value >= 0)
-                    surpriseSaladGrade = value;
-                else
-                    surpriseSaladGrade = -1;
-            }
         }
 
         public bool WelcomeAgain()
--- AwaAssignment2/Program.cs
+++ AwaAssignment2/Program.cs
@@ -7,37 +7,26 @@ namespace awa_assignment_2
 {
     class Program
     {
-        //När du byggt så är det främst .exe filen (inte vshost.exe) samt eventuella .dll-filer som behövs. 
-        //Dessa kan du zipp'a ihop och skicka till en kompis om du vill. Om du använder dig av inställningar så måste .config-filen skickas med.
 
-        //Why error when entering ingredients with the same price or name? Max and min value should be able to be the same.
-        // As well as you could choose the same fruit twice cause you might want 2 of them. And how to fix it? 
-
-        //Create new files/classes from the project: Calculator maybe one class...? So it is possible to create classes without creating new
-        //objects from them. Do the methods in the class static (and public I guess) and then call them
-        //from Main (or from anywhere in another file?) using Classname.MethodName(argument);. Could also make the whole class static and then you 
-        //can't create an object.
-
-        //The code works just fine with the properties declared like this but I don't use the advantage with them. I should declare a private variable in camel Case
-        //and then do the property public with Pascal Case (as below) but then get use of the getters and setters for it to make sense. And if it's a read-only 
-        //property the setter can be removed. And if I need to initialize the setter with an if statement I could do that. And the getter should return the above 
-        //declared private variable in camel Case. The setter also refers to the camel Case variable above.
-        public static string Ingredient { get; set; }
-        public static double Price { get; set; }
-        public static double MaxValue { get; set; }
-        public static double MinValue { get; set; }
-        public static string KeyOfMaxValue { get; set; }
-        public static string KeyOfMinValue { get; set; }
-        public static string SantaString { get; set; }
-        public static double Remainder { get; set; }
-        public static double TotalPrice { get; set; }
-        public static int Counter { get; set; }
-        public static string[] SurpriseIngredients { get; set; }
-        public static char typedLetter { get; set; }
-        public static string[] SurpriseSpices { get; set; }
-        public static string RandomizedSpice { get; set; }
-        public static Dictionary<string, double> ingredients = new Dictionary<string, double>();
-        public static List<string> SurpriseSalad = new List<string>();
+        // Why do I need to declare the fields as static? I use them in static methods and that's what VS complains about but I can't remove static from the 
+        // methods either it seems. Due to the documentation they shouldn't need to be static...?
+        static string ingredient;
+        static double price;
+        static double maxValue;
+        static double minValue;
+        static string keyOfMaxValue;
+        static string keyOfMinValue;
+        static string santaString;
+        static double remainder;
+        static double totalPrice;
+        static int counter;
+        static string surpriseInstructions;
+        static string[] surpriseIngredients;
+        static char typedLetter;
+        static string[] surpriseSpices;
+        static string randomizedSpice;
+        static Dictionary<string, double> ingredients = new Dictionary<string, double>();
+        static List<string> surpriseSalad = new List<string>();
 
         static void Main(string[] args)
         {
@@ -46,7 +35,7 @@ namespace awa_assignment_2
                 WelcomeUser();
                 AskForIngredient();
                 CalculatePrices();
-                Console.WriteLine($"The remainder for Santa to pay is: SEK {Remainder}:-. {SantaString}");
+                Console.WriteLine($"The remainder for Santa to pay is: SEK {remainder}:-. {santaString}");
                 ShowPrices();
                 CreateSurpriseSalad();
                 PresentGuestGrades();
@@ -63,8 +52,9 @@ namespace awa_assignment_2
             Console.WriteLine("Hello and welcome to this fruit salad party!".ToUpper());
             Console.WriteLine("\nOh, but the fruit salad isn't ready yet... in fact we didn't even decide " +
                 "which ingredients we want - let's fix that!\n\nAND TO PREPARE YOUR MINDSET, there will be some extras here and it wont make sense " +
-                "if you don't read all the \"bla bla bla\", so my recommendation is that you do just that :) Oh, and it will take some time!" +
-                "\n\nPlease press Enter to continue...");
+                "if you don't read all the \"bla bla bla\", so my recommendation is that you do just that :) Oh, and it will take some time (aka if you're out " +
+                "of Caffeine - please refill)!" +
+                "\n\nPress Enter to continue...");
             Console.ReadLine();
             Console.Clear();
         }
@@ -76,19 +66,18 @@ namespace awa_assignment_2
             {
                 Console.Write("Enter an ingredient for your fruit salad: ");
                 //Kan man använda Console.ReadKey här i stället?
-                Ingredient = Console.ReadLine();
-                if (Ingredient.Length < 2)
+                ingredient = Console.ReadLine();
+                if (ingredient.Length < 2)
                 {
                     i--;
-                    Console.WriteLine("Are you really making a proper fruit salad? At least two characters are needed " +
-                        "to be a fruit around here...");
+                    Console.WriteLine("Are you really making a proper fruit salad? At least two characters are needed to be a fruit around here...");
                     continue;
                 }
                 Console.Write("Enter price for \"one unit\" of the ingredient (SEK): ");
 
                 try
                 {
-                    Price = Convert.ToDouble(Console.ReadLine());
+                    price = Convert.ToDouble(Console.ReadLine());
                 }
                 catch (FormatException e)
                 {
@@ -99,12 +88,12 @@ namespace awa_assignment_2
                 try
                 {
 
-                ingredients.Add(Ingredient, Price);
+                ingredients.Add(ingredient, price);
                 }
                 catch
                 {
                     i--;
-                    Console.WriteLine("\nThe same ingredient twice - really? Some more imagination please!\n");
+                    Console.WriteLine("The same ingredient twice - really? Some more imagination please!");
                     continue;
                 }
             }
@@ -115,14 +104,14 @@ namespace awa_assignment_2
         {
             Console.Clear();
 
-                MaxValue = ingredients.Values.Max();
-                KeyOfMaxValue = ingredients.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
-                MinValue = ingredients.Values.Min();
-                KeyOfMinValue = ingredients.Aggregate((x, y) => x.Value < y.Value ? x : y).Key;
+                maxValue = ingredients.Values.Max();
+                keyOfMaxValue = ingredients.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
+                minValue = ingredients.Values.Min();
+                keyOfMinValue = ingredients.Aggregate((x, y) => x.Value < y.Value ? x : y).Key;
 
             foreach (KeyValuePair<string, double> item in ingredients)
             {
-                TotalPrice += item.Value;
+                totalPrice += item.Value;
             }
 
             CalculateRemainder();
@@ -132,18 +121,19 @@ namespace awa_assignment_2
         {
 
             Console.WriteLine("It turns out that 2 of your friends will share the cost with you and Santa will pay the remainder - how random!\n");
-            Remainder = TotalPrice % 3;
+            remainder = totalPrice % 3;
 
-            if (Remainder < 1)
-            {
-                SantaString = "\"- That was a good deal for me! I could be a nice guy and still lose nothing :)\" /Santa";
-            }
+            if (remainder < 1)
+                santaString = "\"- That was a good deal for me! I could be a nice guy and still lose nothing :)\" /Santa";
             else
-            {
-                SantaString = "\"- What?? I never gave my consent to this! I'm afraid you should consider this as your pre-given " +
+                santaString = "\"- What?? I never gave my consent to this! I'm afraid you should consider this as your pre-given " +
                     "2021 Christmas gift...\" /Santa";
-            }
-            return (Remainder, SantaString);
+
+            // Alternativt syntax med samma resultat
+            //santaString = (remainder < 1) ? "\"- That was a good deal for me! I could be a nice guy and still lose nothing :)\" /Santa" : "\"- What?? I never gave my consent to this! I'm afraid you should consider this as your pre-given " +
+            //        "2021 Christmas gift...\" /Santa";
+
+            return (remainder, santaString);
         }
 
         static void ShowPrices()
@@ -157,20 +147,20 @@ namespace awa_assignment_2
         {
             Console.WriteLine("\nValuable facts about your fruit salad:".ToUpper());
             Console.WriteLine();
-            Console.WriteLine($"Total price: SEK {TotalPrice}:-");
+            Console.WriteLine($"Total price: SEK {totalPrice}:-");
             Console.WriteLine();
         }
         static void ShowMostExpensive()
         {
-            Console.WriteLine($"Most expensive ingredient: {KeyOfMaxValue}\nPrice: SEK {MaxValue}:-");
+            Console.WriteLine($"Most expensive ingredient: {keyOfMaxValue}\nPrice: SEK {maxValue}:-");
             Console.WriteLine();
         }
         static void ShowCheapest()
         {
-            Console.WriteLine($"Cheapest ingredient: {KeyOfMinValue}\nPrice: SEK {MinValue}:-");
+            Console.WriteLine($"Cheapest ingredient: {keyOfMinValue}\nPrice: SEK {minValue}:-");
             Console.WriteLine();
             Console.WriteLine("Bon appéttit!\n\nOh, but wait...".ToUpper());
-            Console.WriteLine("...something peculiar just occured!\n\nPlease press Enter to find out more...");
+            Console.WriteLine("...something peculiar just occured!\n\nPress Enter to find out more...");
             Console.ReadLine();
         }
 
@@ -191,7 +181,7 @@ namespace awa_assignment_2
             Console.WriteLine("When you sent this fruit salad recipe to your fruit salad teacher he identified some areas of improvement. " +
                 "His biggest concern was that it seemed a bit too..." +
                 " conventional. Your guests' taste buds might get bored, and we can't take that risk!\n\nLet's create a small but OH SO " +
-                "DELICIOUS surprise salad instead, with fresh ingredients from all over the world!\n\nPlease press Enter to continue...\n");
+                "DELICIOUS surprise salad instead, with fresh ingredients from all over the world!\n\nPress Enter to continue...\n");
             Console.ReadLine();
             Console.Clear();
 
@@ -202,7 +192,7 @@ namespace awa_assignment_2
 
             PresentGuestList();
 
-            Console.WriteLine("Please press Enter to start creating your surprise salad!".ToUpper());
+            Console.WriteLine("Press Enter to start creating your surprise salad!".ToUpper());
             Console.ReadLine();
             Console.Clear();
         }
@@ -211,42 +201,46 @@ namespace awa_assignment_2
         {
             try
             {
-                SurpriseIngredients = File.ReadAllLines(@"C:\Users\ingri\OneDrive\Desktop\surpriseIngredients.txt");
+                surpriseIngredients = File.ReadAllLines(@"C:\Users\ingri\OneDrive\Desktop\surpriseIngredients.txt");
             }
             catch (FileNotFoundException)
             {
-                SurpriseIngredients = new string[] { "Chayote", "Kiwano", "Dragon fruit", "Feijoa",
+                surpriseIngredients = new string[] { "Chayote", "Kiwano", "Dragon fruit", "Feijoa",
                     "Tamarillo", "Loquat", "Jabuticaba", "Monstera deliciosa", "Rambutan", "Pacay", "Starfruit", "Atemoya",
                 "Buddha's hand", "Guanabana", "Noni fruit" };
             }
 
-            SurpriseSpices = new string[] { "vata churna", "dill",
+            surpriseSpices = new string[] { "vata churna", "dill",
                 "thyme", "catnip", "fenugreek", "garlic", "kawakawa", "savory" };
         }
 
         static void GetUserInput()
         {
+            Console.WriteLine($"Letters are fruit! OBS! Some letters wont give you any " +
+                            $"fruit (such is life I guess)... " +
+                            $"In that case, don't lose hope - be persistent and keep typing!\n");
             do
             {
-                switch (SurpriseSalad.Count())
+                switch (surpriseSalad.Count())
                 {
                     case 0:
-                        Counter = 1;
+                        counter = 1;
+                        surpriseInstructions = $"Type any letter to get ingredient number {counter}: ";
                         break;
                     case 1:
-                        Counter = 2;
+                        counter = 2;
+                        surpriseInstructions = $"Type another letter to get ingredient number {counter}: ";
                         break;
                     case 2:
-                        Counter = 3;
+                        counter = 3;
+                        surpriseInstructions = $"Type another letter to get ingredient number {counter}: ";
                         break;
                     default:
                         break;
                 }
                 try
                 {
-                    Console.WriteLine($"Please type any letter to get ingredient number {Counter}\n\nOBS! Some letters wont give you any fruit " +
-                        $"(such is life I guess)... " +
-                        $"In that case, don't lose hope - be persistent and keep typing!");
+                    Console.Write($"{surpriseInstructions}");
                     typedLetter = char.Parse(Console.ReadLine().ToUpper());
                 }
                 catch (FormatException e)
@@ -255,22 +249,22 @@ namespace awa_assignment_2
                     continue;
                 }
 
-                foreach (string fruit in SurpriseIngredients)
+                foreach (string fruit in surpriseIngredients)
                 {
                     if (fruit.StartsWith(typedLetter))
                     {
-                        Console.WriteLine($"\"{typedLetter}\" gives ju {fruit.ToUpper()}");
-                        SurpriseSalad.Add(fruit);
+                        Console.WriteLine($"\n\"{typedLetter}\" gives ju {fruit.ToUpper()}");
+                        surpriseSalad.Add(fruit);
                         Console.WriteLine();
                     }
                 }
 
-            } while (SurpriseSalad.Count < 3); ;
+            } while (surpriseSalad.Count < 3); ;
         }
 
         static void MakeGameFlow2()
         {
-            Console.WriteLine("You've got all your fruits! Please press Enter to continue...".ToUpper());
+            Console.WriteLine("You've got all your fruits! Press Enter to continue...".ToUpper());
             Console.ReadLine();
             Console.Clear();
         }
@@ -278,14 +272,14 @@ namespace awa_assignment_2
         static void GetRandomSpice()
         {
             Console.WriteLine("And now we'll add a generous amount of a random spice to your surprise salad!".ToUpper());
-            Console.WriteLine("\nPlease press Enter to find out which one...");
+            Console.WriteLine("\nPress Enter to find out which one...");
             Console.ReadLine();
             Console.Clear();
 
             Random random = new Random();
 
-            RandomizedSpice = SurpriseSpices[random.Next(0, SurpriseSpices.Length)];
-            Console.WriteLine($"Your spice of choice is: {RandomizedSpice.ToUpper()}\n");
+            randomizedSpice = surpriseSpices[random.Next(0, surpriseSpices.Length)];
+            Console.WriteLine($"Your spice of choice is: {randomizedSpice.ToUpper()}\n");
         }
 
         static void PresentSurpriseSalad()
@@ -294,12 +288,12 @@ namespace awa_assignment_2
             " shipped from all over the world especially for you, the price will be the biggest surprise of all! :)\n");
             Console.WriteLine("Your surprise salad is a blend made to perfection out of the following ingredients:\n".ToUpper());
 
-            foreach (string fruit in SurpriseSalad)
+            foreach (string fruit in surpriseSalad)
             {
                 Console.WriteLine(fruit.ToUpper());
             }
 
-            Console.WriteLine(RandomizedSpice.ToUpper());
+            Console.WriteLine(randomizedSpice.ToUpper());
             Console.WriteLine();
             Console.WriteLine("SURPRISE PRICE: SEK 3 275 483:-\n");
         }
@@ -319,7 +313,7 @@ namespace awa_assignment_2
         private static void PresentGuestGrades()
         {
             Console.WriteLine("Okay... please sit down... the moment of truth has arrived. Each of your guests is now ready to present to " +
-                "you the grade they have given your surprise salad.\n\nPlease press Enter to get your grades (scale 0-5)");
+                "you the grade they have given your surprise salad.\n\nPress Enter to get your grades (scale 0-5)");
             Console.ReadLine();
             Console.Clear();
             Console.WriteLine("YOUR GRADES: \n");
@@ -329,7 +323,7 @@ namespace awa_assignment_2
             Guest guest2 = new Guest("Miss Lemon", 10, "Missed some balancing sour.");
             Guest guest4 = new Guest("Mademoiselle E. Petit", 5, "Mmmmmmmmmmm...");
 
-            Console.WriteLine("I hope you are satisfied with your grades. Please press Enter to continue...");
+            Console.WriteLine("I hope you are satisfied with your grades. Press Enter to continue...");
             Console.ReadLine();
         }
 
